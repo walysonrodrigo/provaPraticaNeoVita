4062c6bdf36e8b6f2527513d000c40a47596be6f
walysonrodrigo <walysonrodrigo@gmail.com>
2023-07-12 18:17:54 -0300

    feat: migration 2023-07-11-143436_News

---
+++ app/Database/Migrations/2023-07-11-143436_News.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class News extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'content' => [
+                'type' => 'TEXT',
+            ],
+            'author' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'publication_date' => [
+                'type' => 'DATE',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('news');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('news');
+    }
+}
